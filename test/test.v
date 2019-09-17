74d02782721ce50c28fdd19b58bc38581f52e3d1
Clemens Schmid <clemens@nevrome.de>
2019-09-17 23:00:07 +0200

    added a function to create an array of random numbers

--- test/test.v
+++ test/test.v
@@ -1,11 +1,19 @@
 module main
 
 import vconrand
+import rand
+import time
 
 fn main() {
 
+  rand.seed(time.now().uni)
+
   println('vconrand.runif(384.12, 399.54) = ' +
     vconrand.runif(384.12, 399.54).str()
+  )
+
+  println('vconrand.runif_n(5, 384.12, 399.54) = ' +
+    vconrand.runif_n(5, 384.12, 399.54).str()
   )
 
 }
--- vconrand.v
+++ vconrand.v
@@ -1,16 +1,22 @@
 module vconrand
 
 import rand
-import time
 
 pub fn runif(min f64, max f64) f64 {
-
-  rand.seed(time.now().uni)
 
   range := max - min
   div := f64(C.RAND_MAX) / range
   res := min + (f64(rand.next(C.RAND_MAX)) / div)
 
   return res
+}
 
+pub fn runif_n(n int, min f64, max f64) []f64 {
+
+  mut res := [f64(0)].repeat(n)
+  for i := 0; i < n; i++ {
+    res[i] = runif(min, max)
+  }
+
+  return res
 }

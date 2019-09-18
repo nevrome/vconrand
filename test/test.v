59e24c28ecc2efae03f318278a5dff1081b819aa
Clemens Schmid <clemens@nevrome.de>
2019-09-18 12:13:21 +0200

    small modifications of test code

--- test/test.v
+++ test/test.v
@@ -1,5 +1,3 @@
-module main
-
 import vconrand
 import rand
 import time
@@ -8,18 +6,18 @@ fn main() {
 
   rand.seed(time.now().uni)
 
+  // unif
   println('vconrand.runif(384.12, 399.54) = ' +
     vconrand.runif(384.12, 399.54).str()
   )
-
   println('vconrand.runif_n(5, 384.12, 399.54) = ' +
     vconrand.runif_n(5, 384.12, 399.54).str()
   )
 
+  // norm
   println('vconrand.rnorm(14.81, 2.73) = ' +
     vconrand.rnorm(14.81, 2.73).str()
   )
-
   println('vconrand.rnorm_n(5, 14.81, 2.73) = ' +
     vconrand.rnorm_n(5, 14.81, 2.73).str()
   )
